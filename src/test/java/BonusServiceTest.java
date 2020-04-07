5607c1442fded1498d87dd5b95994d474acc394a
ZhukovaAnna <zhukova.anna.home@gmail.com>
2020-04-07 21:59:00 +0300

    First commit

--- src/test/java/BonusServiceTest.java
+++ src/test/java/BonusServiceTest.java
@@ -7,15 +7,12 @@ class BonusServiceTest {
   void shouldCalculateForRegisteredAndUnderLimit() {
     BonusService service = new BonusService();
 
-    // подготавливаем данные:
     long amount = 1000_60;
     boolean registered = true;
     long expected = 30;
 
-    // вызываем целевой метод:
     long actual = service.calculate(amount, registered);
 
-    // производим проверку (сравниваем ожидаемый и фактический):
     assertEquals(expected, actual);
   }
 
@@ -23,45 +20,36 @@ class BonusServiceTest {
   void shouldCalculateForRegisteredAndOverLimit() {
     BonusService service = new BonusService();
 
-    // подготавливаем данные:
     long amount = 1_000_000_60;
     boolean registered = true;
     long expected = 500;
 
-    // вызываем целевой метод:
     long actual = service.calculate(amount, registered);
 
-    // производим проверку (сравниваем ожидаемый и фактический):
     assertEquals(expected, actual);
   }
   @org.junit.jupiter.api.Test
   void shouldCalculateForUnRegisteredAndUnderLimit() {
     BonusService service = new BonusService();
 
-    // подготавливаем данные:
     long amount = 1000_60;
     boolean registered = false;
     long expected = 10;
 
-    // вызываем целевой метод:
     long actual = service.calculate(amount, registered);
 
-    // производим проверку (сравниваем ожидаемый и фактический):
     assertEquals(expected, actual);
   }
   @org.junit.jupiter.api.Test
   void shouldCalculateForUnRegisteredAndOverLimit() {
     BonusService service = new BonusService();
 
-    // подготавливаем данные:
     long amount = 1_000_000_60;
-    boolean registered = true;
+    boolean registered = false;
     long expected = 500;
 
-    // вызываем целевой метод:
     long actual = service.calculate(amount, registered);
 
-    // производим проверку (сравниваем ожидаемый и фактический):
     assertEquals(expected, actual);
   }
 }
